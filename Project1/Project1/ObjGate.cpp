49f8caadb6112d1c86a29fc69aecf76ac8c6f954
student <student@DEV2-19>
2018-01-24 18:53:18 +0900

    門　門　門

--- Project1/Project1/ObjGate.cpp
+++ Project1/Project1/ObjGate.cpp
@@ -10,25 +10,29 @@
 #include "ObjGate.h"
 #include "Objhero.h"
 
+//extern CObjhero* objh;
+
 //使用するネームスペース
 using namespace GameL;
+
+//ブロック＆主人公切り替え false=妹用 true=兄用
+extern bool g_hero_change;
 
 //----------------------------------------------
 
 //門ブロック
 
-//ブロック＆主人公切り替え false=妹用 true=兄用
-extern bool g_hero_change;
-
-CObjGateBlock::CObjGateBlock()
+CObjGate::CObjGate()
 {
 }
 //イニシャライズ
-void CObjGateBlock::Init()
+void CObjGate::Init()
 {
+	gb_map_x = 14;
+	gb_x = 0.0f;
 }
 //アクション
-void CObjGateBlock::Action()
+void CObjGate::Action()
 {
 	//兄主人公の位置を取得
 	CObjhero* hero = (CObjhero*)Objs::GetObj(OBJ_HERO);
@@ -40,101 +44,93 @@ void CObjGateBlock::Action()
 	float hx2 = hero2->GetX();
 	float hy2 = hero2->GetY();
 
-	/*if (g_hero_change == true)
+	//主人公スクロールライン
+	if (g_hero_change == true)
 	{
-		//兄後方スクロールライン
-		if (hx < 80)
+		//兄スクロールライン
+		if (hx < 80|| hx > 400)
 		{
-			hero->SetX(80);						//主人公はラインを超えないようにする
-			m_block_scroll -= hero->GetVX();	//主人公が本来動くべき分の値をm_block_scrollに加える
+			gb_x -= hero->GetVX();
 		}
-
-		//兄前方スクロールライン
-		if (hx > 400)
+	}
+	else
+	{
+		//妹スクロールライン
+		if (hx2 < 80|| hx2 > 400)
 		{
-			hero->SetX(400);					//主人公はラインを超えないようにする
-			m_block_scroll -= hero->GetVX();	//主人公が本来動くべき分の値をm_block_scrollに加える
+			gb_x -= hero2->GetVX();
 		}
 	}
 
+	//当たり判定-------------------------------------------
+
+	//要素番号を座標に変更
+	float x = gb_map_x * 64;
+
+	//主人公とブロックの当たり判定
+	
+
+}
+//ドロー
+void CObjGate::Draw()
+{
+	//カラー情報
+	float c[4] = { 1.0f,1.0f,1.0f ,1.0f };
+
+	RECT_F src;	//描画元切り取り位置
+	RECT_F dst;	//描画元表示位置
+
+	int i;
+
+	//門ブロック----------------------------------------------------
+	for (i = 0; i < 8; i++)
+	{
+		//切り取り位置の設定
+		src.m_top = 0.0f;
+		src.m_left = 0.0f;
+		src.m_right = 64.0f;
+		src.m_bottom = 64.0f;
+		//表示位置の設定
+		dst.m_top    = ((i * 2) * 32.0f);
+		dst.m_left   =  (gb_map_x            * 32.0f)       + gb_x;
+		dst.m_right  = ((gb_map_x            * 32.0f) + 64) + gb_x;
+		dst.m_bottom = ((i * 2) * 32.0f) + 64;
+
+		Draw::Draw(8, &src, &dst, c, 0.0f);
+	}
+
+	//兄閉門ブロック------------------------------------------------
+	if (g_hero_change == true)
+	{
+		//切り取り位置の設定
+		src.m_top = 0.0f;
+		src.m_left = 64.0f;
+		src.m_right = 128.0f;
+		src.m_bottom = 64.0f;
+		//表示位置の設定
+		dst.m_top = ((i * 2) * 32.0f);
+		dst.m_left = (gb_map_x * 32.0f) + gb_x;
+		dst.m_right = ((gb_map_x * 32.0f) + 64) + gb_x;
+		dst.m_bottom = ((i * 2) * 32.0f) + 64;
+
+		Draw::Draw(8, &src, &dst, c, 0.0f);
+	}
+	//妹閉門ブロック------------------------------------------------
 	else
 	{
-		//妹後方スクロールライン
-		if (hx2 < 80)
-		{
-			hero2->SetX(80);						//主人公はラインを超えないようにする
-			m_block_scroll -= hero2->GetVX();	//主人公が本来動くべき分の値をm_block_scrollに加える
-		}
+		//切り取り位置の設定
+		src.m_top = 0.0f;
+		src.m_left = 128.0f;
+		src.m_right = 192.0f;
+		src.m_bottom = 64.0f;
+		//表示位置の設定
+		dst.m_top = ((i * 2) * 32.0f);
+		dst.m_left = (gb_map_x * 32.0f) + gb_x;
+		dst.m_right = ((gb_map_x * 32.0f) + 64) + gb_x;
+		dst.m_bottom = ((i * 2) * 32.0f) + 64;
 
-		//妹前方スクロールライン
-		if (hx2 > 400)
-		{
-			hero2->SetX(400);					//主人公はラインを超えないようにする
-			m_block_scroll -= hero2->GetVX();	//主人公が本来動くべき分の値をm_block_scrollに加える
-		}
-	}*/
-}
-//ドロー
-void CObjGateBlock::Draw()
-{
-	//カラー情報
-	float c[4] = { 1.0f,1.0f,1.0f ,1.0f };
-
-	RECT_F src;	//描画元切り取り位置
-	RECT_F dst;	//描画元表示位置
-
-	//切り取り位置の設定
-	src.m_top = 0.0f;
-	src.m_left = 0.0f;
-	src.m_right = 64.0f;
-	src.m_bottom = 64.0f;
-
-	//表示位置の設定
-	dst.m_top = 448.0f;
-	dst.m_left = 320.0f;
-	dst.m_right = 384.0f;
-	dst.m_bottom = 512.0f;
-
-	Draw::Draw(15, &src, &dst, c, 0.0f);
-}
-
-//----------------------------------------------
-
-//開門 左
-
-CObjGateOpenLeft::CObjGateOpenLeft()
-{
-}
-//イニシャライズ
-void CObjGateOpenLeft::Init()
-{
-}
-//アクション
-void CObjGateOpenLeft::Action()
-{
-}
-//ドロー
-void CObjGateOpenLeft::Draw()
-{
-	//カラー情報
-	float c[4] = { 1.0f,1.0f,1.0f ,1.0f };
-
-	RECT_F src;	//描画元切り取り位置
-	RECT_F dst;	//描画元表示位置
-
-	//切り取り位置の設定
-	src.m_top = 0.0f;
-	src.m_left = 0.0f;
-	src.m_right = 64.0f;
-	src.m_bottom = 64.0f;
-
-	//表示位置の設定
-	dst.m_top = 512.0f;
-	dst.m_left = 320.0f;
-	dst.m_right = 384.0f;
-	dst.m_bottom = 576.0f;
-
-	Draw::Draw(8, &src, &dst, c, 0.0f);
+		Draw::Draw(8, &src, &dst, c, 0.0f);
+	}
 }
 
 //----------------------------------------------
@@ -171,10 +167,10 @@ void CObjGateOpenRight::Draw()
 	src.m_bottom = 64.0f;
 
 	//表示位置の設定
-	dst.m_top = 512.0f;
+	/*dst.m_top = 512.0f;
 	dst.m_left = 320.0f;
 	dst.m_right = 384.0f;
-	dst.m_bottom = 576.0f;
+	dst.m_bottom = 576.0f;*/
 
 	Draw::Draw(9, &src, &dst, c, 0.0f);
 }
