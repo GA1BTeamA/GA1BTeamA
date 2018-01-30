3d90f6548aeed886facd552edcc06d233695fa97
student <student@DEV2-19>
2018-01-30 15:00:55 +0900

    jkdf

--- Project1/Project1/ObjBackground.h
+++ Project1/Project1/ObjBackground.h
@@ -15,8 +15,10 @@ public:
 	void Draw();	//ドロー
 private:
 	float m_scroll;  //左右スクロール用
-	float m_x1;  //背景1の位置情報
-	float m_x2;  //背景2の位置情報
+	float bm_x1;  //兄背景1の位置情報
+	float bm_x2;  //兄背景2の位置情報
+	float sm_x1;  //妹背景1の位置情報
+	float sm_x2;  //妹背景2の位置情報
 	bool m_key_flag;
 
 	//主人公の位置を取得
